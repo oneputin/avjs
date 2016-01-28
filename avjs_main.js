7256538eb2d30c9578ebbfb209e39d506f56d8e8
oneputin <git@iimaps.de>
2016-01-28 10:35:46 +0100

    update location of testdata

--- avjs_main.js
+++ avjs_main.js
@@ -8,43 +8,32 @@ var removeDeps = function(av3Json, params) {
 }
 // Read and Transform an AV3-ODB into a "equivalent" JSON
 // var file = 'explode.apr';
-var file   = 'av3_explore.apr';
+var file   = "samples/av3_aveexport.apr";
 var params = {file: file};
 
-if (0) { 
+if (0) {	// create a json-translation of file 
 	params["main"] = avjs.save;	
-	params["file"] = "odb/gia_base.avx";	
-	// params["file"] = "odb/gia_app.avx"; 
-	// params["file"] = "odb/geoproc.avx"; 	
-	// params["file"] = "odb/inetmap.avx"; 	
+	params["file"] = "samples/gia_base.avx";	
 }
 
-if (0) {	
+if (0) {	// extract av3.scripts-files(*.ave) into subdirectories  
 	params["main"] = avjs.scripts;	
 	params["folder"] = "test";
 	params["scriptext"] = "ave";
-	params["file"] = "odb/inetmap.avx"; 	
+	// params["file"] = "samples/gia_base.avx"; 	
 	// params["propkeys"] = ["Source", "Name"]; 
+	// params["show"] = true; 	
 }
 
-if (0) {
-	params["main"] = avjs.scripts;	
-	params["file"] = "odb/inetmap.avx"; 	
-	params["file"] = "odb/gia_app.avx"; 
-	params["file"] = "odb/av3_aveexport.apr"; 
-	params["show"] = true; 	
-}
-
-if (1) {
+if (1) {	// 
 	params["main"] = removeDeps; 
-	params["file"] = "odb/inetmap.avx";	
-	params["file"] = "odb/geoproc.avx";	
+	// params["file"] = "samples/gia_base.avx";	
 	params["suffix"] = "_x" ; 
-	// params["delname"] = "gia_sedit.avx"; // name of extension to delete 
+	// params["delname"] = "gia_sedit.avx"; // name of selected extension to delete 
 }
 
-if (0) {
-	params["file"] = "odb/gia_base.avx";	
+if (0) {	// 
+	params["file"] = "samples/gia_base.avx";	
 	params["main"] = avjs.export;
 	params["suffix"] = "_x" ; 
 }
